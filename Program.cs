2fa690ff4fd710a643300bc29e983b6fbc12a48c
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 12:57:55 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boucle7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int numb;
+            for(numb = 1; numb <= 10; numb++)
+            {
+                Console.WriteLine(numb);
+            }
+        }
+    }
+}
