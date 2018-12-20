76aa2868d4b778fce8ff83143b1c0142a788682d
MickaelHm <harmeau.mickael@gmail.com>
2018-12-20 15:27:17 +0100

    ajout commentaire

--- Program.cs
+++ Program.cs
@@ -11,11 +11,16 @@ namespace boucle7
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            int numb;
-            for(numb = 1; numb <= 10; numb++)
+            int counter;
+            for (counter = 0; counter < 10; counter++)//variable, condition, incrémentation | on peut remplacer counter par i
             {
-                Console.WriteLine(numb);
+                Console.WriteLine(numbers[counter]);//tableau[indice]
             }
+            /*Méthode foreach
+            foreach (int number in numbers.Take(10)) Récupère les10 premiers nombre grace à la fonction .take()
+            {
+                Console.WriteLine(number);
+            }*/
         }
     }
 }
